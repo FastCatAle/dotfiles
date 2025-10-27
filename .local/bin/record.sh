20af42f404dfd0ea6c146286c3065d7552d99df8
FastCatAle <93169985+FastCatAle@users.noreply.github.com>
2025-10-26 20:55:25 -0500

    I forgot this...

--- .local/bin/record.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-wf-recorder -f $HOME/Videos/recordings/recording_vaapi.mkv -c h264_vaapi -d /dev/dri/renderD128
--- .local/bin/wmenu.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-wmenu-run -f "IBM Plex Mono 10" \
-	-N '171414' -n 'f7f3f2' \
-	-M '171414' -m 'f7f3f2' \
-	-S 'f7f3f2' -s '171414'
